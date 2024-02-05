5b3bea115349931784f6603b4dcfe48989d9b4cc
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-05 07:53:23 +0000

    Modified patching.

--- sh/getSource.sh
+++ sh/getSource.sh
@@ -2,10 +2,17 @@
 if [ -d "./source" ]; then
 	echo "Updating remote source..."
 	cd source
+	if [ "$1" != "" ]; then
+		git checkout "$1"
+	fi
 	git pull
 	git rebase
 else
 	echo "No source available locally. Fetching..."
-	git clone https://github.com/Sollace/Unicopia.git source
+	if [ "$1" != "" ]; then
+		git clone https://github.com/Sollace/Unicopia.git source --branch "$1"
+	else
+		git clone https://github.com/Sollace/Unicopia.git source
+	fi
 fi
 exit
